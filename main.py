416e98e74b38b18c7e74e36405e0a5bf7d769948
Muspi Merol <wjj123345567@icloud.com>
2022-05-19 10:22:45 +0800

    project structure built

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from starlette.staticfiles import StaticFiles
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="./static"))
+
+
+class Record(BaseModel):
+    class_: str
+    data: bytes
+
+
+@app.post("/upload")
+def add_record(record: Record):
+    print(record)
